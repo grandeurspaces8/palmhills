18575d3b96202fb66d5f58aea4e1285970a7f637
Nadaabdelrahman199 <grandeurspaces8@gmail.com>
2026-04-13 18:39:25 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname)
+    return config
   },
 }
 
